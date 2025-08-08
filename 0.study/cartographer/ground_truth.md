01e150e573738133a23ee57e12ef256421f07838
tong <1564264931@qq.com>
2025-08-08 15:18:50 +0800

    temp

--- 0.study/cartographer/ground_truth.md
+++ 0.study/cartographer/ground_truth.md
@@ -1,3 +1,4 @@
 # ground_truth
 
 * #TODO: ...
+用于评估 SLAM 结果
--- 0.study/script/gen_puml.py
+++ 0.study/script/gen_puml.py
@@ -16,7 +16,7 @@ workpath = str(Path(__file__).resolve().parent.parent.parent)
 logging.debug(f"workpath: {workpath}")
 
 
-g_plantuml_jar=workpath +'/0.study/assets/plantuml-1.2025.4.jar'
+g_plantuml_jar = workpath + "/0.study/assets/plantuml-1.2025.4.jar"
 
 
 def gen_puml(input_files, output_file):
@@ -33,8 +33,15 @@ def gen_puml(input_files, output_file):
 
     logging.debug(f"cmd: {cmd}")
     ret = os.system(cmd)
-    
-    cmd = 'java -jar '+g_plantuml_jar+' -tsvg -o '+str(Path(output_file).resolve().parent)+'  ' +output_file
+
+    cmd = (
+        "java -jar "
+        + g_plantuml_jar
+        + " -tsvg -o "
+        + str(Path(output_file).resolve().parent)
+        + "  "
+        + output_file
+    )
     logging.debug(f"cmd: {cmd}")
     ret |= os.system(cmd)
 
@@ -58,19 +65,19 @@ def handle_common():
 
     # gen blocking_queue.puml
     input_files = glob.glob(os.path.join(common_path, "internal", "*_queue.h"))
-    output_file = common_out_path +"/blocking_queue.puml"
+    output_file = common_out_path + "/blocking_queue.puml"
     gen_puml(input_files, output_file)
-    
+
     # gen file_resolver.puml
     input_files = glob.glob(os.path.join(common_path, "*file_resolver.h"))
     input_files += glob.glob(os.path.join(common_path, "lua_parameter_dictionary.h"))
-    output_file = common_out_path +"/file_resolver.puml"
+    output_file = common_out_path + "/file_resolver.puml"
     gen_puml(input_files, output_file)
-    
+
     # gen thread_pool.puml
     input_files = glob.glob(os.path.join(common_path, "thread_pool.h"))
     input_files += glob.glob(os.path.join(common_path, "task.h"))
-    output_file = common_out_path +"/thread_pool.puml"
+    output_file = common_out_path + "/thread_pool.puml"
     gen_puml(input_files, output_file)
 
 
@@ -83,11 +90,20 @@ def handle_metrics():
         workpath, "0.study", "cartographer", "assets", "puml", "metrics"
     )
     os.makedirs(metrics_out_path, exist_ok=True)
-    
+
     # gen metrics.puml
     input_files = glob.glob(os.path.join(metrics_path, "*.h"))
-    input_files += glob.glob(os.path.join(workpath, "cartographer", "cloud", "metrics","prometheus", "family_factory.*"))
-    output_file = metrics_out_path +"/metrics.puml"
+    input_files += glob.glob(
+        os.path.join(
+            workpath,
+            "cartographer",
+            "cloud",
+            "metrics",
+            "prometheus",
+            "family_factory.*",
+        )
+    )
+    output_file = metrics_out_path + "/metrics.puml"
     gen_puml(input_files, output_file)
 
 
@@ -100,11 +116,12 @@ def handle_sensor():
         workpath, "0.study", "cartographer", "assets", "puml", "sensor"
     )
     os.makedirs(sensor_out_path, exist_ok=True)
-    
+
     # gen sensor.puml
-    input_files = glob.glob(os.path.join(sensor_path, "*.h"),recursive=True)
-    output_file = sensor_out_path +"/sensor.puml"
+    input_files = glob.glob(os.path.join(sensor_path, "*.h"), recursive=True)
+    output_file = sensor_out_path + "/sensor.puml"
     gen_puml(input_files, output_file)
+
 
 def handle_io():
     """
@@ -115,10 +132,26 @@ def handle_io():
         workpath, "0.study", "cartographer", "assets", "puml", "io"
     )
     os.makedirs(io_out_path, exist_ok=True)
-    
+
     # gen io.puml
-    input_files = glob.glob(os.path.join(io_path, "*.h"),recursive=True)
-    output_file = io_out_path +"/io.puml"
+    input_files = glob.glob(os.path.join(io_path, "*.h"), recursive=True)
+    output_file = io_out_path + "/io.puml"
+    gen_puml(input_files, output_file)
+
+
+def handle_mapping():
+    """
+    Handle the mapping case.
+    """
+    mapping_path = os.path.join(workpath, "cartographer", "mapping")
+    mapping_out_path = os.path.join(
+        workpath, "0.study", "cartographer", "assets", "puml", "mapping"
+    )
+    os.makedirs(mapping_out_path, exist_ok=True)
+
+    # gen mapping.puml
+    input_files = glob.glob(os.path.join(mapping_path, "**/*.h"), recursive=True)
+    output_file = mapping_out_path + "/mapping.puml"
     gen_puml(input_files, output_file)
 
 
@@ -128,6 +161,7 @@ if __name__ == "__main__":
     parser.add_argument("--metrics", help="", type=bool, default=False)
     parser.add_argument("--sensor", help="", type=bool, default=False)
     parser.add_argument("--io", help="", type=bool, default=False)
+    parser.add_argument("--mapping", help="", type=bool, default=False)
 
     if parser.parse_args().common:
         handle_common()
@@ -137,6 +171,8 @@ if __name__ == "__main__":
         handle_sensor()
     elif parser.parse_args().io:
         handle_io()
+    elif parser.parse_args().mapping:
+        handle_mapping()
 
 
 # hpp2plantuml -i  ../../cartographer/common/internal/blocking_queue.h -o common.puml
